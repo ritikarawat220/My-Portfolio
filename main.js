d53f099d9e09688501b4508746bfd9810e5a4672
Ritika Rawat <Ritikarawat220@gmail.com>
2023-01-31 17:44:36 +0530

    Modified main.js & style.css files

--- main.js
+++ main.js
@@ -1,38 +1,50 @@
-const hamburger = document.querySelector('.hamburger'); //container containing menu and nav links
-const openMenu = document.querySelector('.mobile-option'); //menu icon
-const extBtn = document.querySelector('.exit'); //exit button
+const navSectionOne = document.querySelector('.info'); // container containing menu and nav links
+const navSectionTwo = document.querySelector('.menu-icon'); // menu-icon
 const links = document.querySelectorAll('.co'); // navigation class list
 const sections = document.querySelectorAll('.sections'); // sections classlist
 const windowPopup = document.querySelector('.window-popup');// container containiing the popup
 const bodyButtons = document.querySelector('.all-container'); // general body contain
 const PortfolioContainer = document.getElementById('portfolio'); // container for the cards
 
-function show() {
-  openMenu.style.display = 'block';
-  openMenu.classList.toggle('open');
-  hamburger.style.visibility = 'hidden';
-  extBtn.style.visibility = 'visible';
+// display the popup menu bar
+function menuTransition() {
+  if (navSectionOne.classList.contains('phone')) {
+    navSectionOne.classList.remove('phone');
+  } else {
+    navSectionOne.classList.add('phone');
+    sections.forEach((sect) => {
+      sect.classList.toggle('active');
+    });
+  }
+}
+// once click on the men-icon
+navSectionTwo.addEventListener('click', menuTransition);
+
+// hides the menu bar
+function removeMenu() {
+  if (navSectionOne.classList.contains('phone')) {
+    navSectionOne.classList.remove('phone');
+    sections.forEach((sect) => {
+      sect.classList.toggle('active');
+    });
+  }
 }
 
-function close() {
-  openMenu.style.display = 'none';
-  openMenu.classList.remove('open');
-  hamburger.style.visibility = 'visible';
-  extBtn.style.visibility = 'hidden';
-}
+links.forEach((link) => {
+  link.addEventListener('click', removeMenu);
+});
 
-hamburger.addEventListener('click', show);
-extBtn.addEventListener('click', close);
-
-openMenu.addEventListener('click', close);
-
+// You need to update the main page so that the projects section
+// is created dynamically using the information stored in that
+// JavaScript object. Remember, all of the HTML in that section is created when the page loads.
+// order class is to switch them , move image to the right
 const cards = [
   {
     id: 'cardOne',
     title: 'Tonic',
     education: ['CANOPY', '&nbsp; Back End Dev', '&nbsp; 2015'],
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    technologies: ['html5', 'Ruby on rails', 'css', 'javascript'],
+    technologies: ['html5', 'css', 'javascript'],
     image: {
       mainImg: 'portfolio/Nature.png',
       counter: './portfolio/Counter.png',
@@ -45,7 +57,7 @@ const cards = [
     title: 'Multi-Post Stories',
     education: ['FACEBOOK', '&nbsp; Back End Dev', '&nbsp; 2015'],
     description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
-    technologies: ['html', 'Ruby on rails', 'css', 'javascript'],
+    technologies: ['html', 'css', 'javascript'],
     image: {
       mainImg: 'portfolio/profes...png',
       counter: './Portfolio/Counter.png',
@@ -59,7 +71,7 @@ const cards = [
     title: 'Facebook 360',
     education: ['CANOPY', '&nbsp; Back End Dev', '&nbsp; 2015'],
     description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
-    technologies: ['html', 'Ruby on rails', 'css', 'javascript'],
+    technologies: ['html', 'css', 'javascript'],
     image: {
       mainImg: 'portfolio/Snapshoot Portfolio.png',
       counter: './portfolio/Counter.png',
@@ -72,7 +84,7 @@ const cards = [
     title: 'Uber Navigation',
     education: ['Uber', '&nbsp; Back End Dev', '&nbsp; 2015'],
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    technologies: ['html', 'Ruby on rails', 'css', 'javascript'],
+    technologies: ['html', 'css', 'javascript'],
     image: {
       mainImg: 'portfolio/availa.png',
       counter: './portfolio/Counter.png',
@@ -179,3 +191,134 @@ const popup = [
     },
   },
 ];
+
+// all of the HTML in that section is created when the page loads.
+cards.forEach((object) => {
+  PortfolioContainer.innerHTML += `<div class="card">
+  <img class="img ${object.orderClas}" src="${object.image.mainImg}" alt="Snapshoot Portfolio image">
+  <div class="sec-content">
+  <h2 class="ton-del">
+   ${object.title}
+  </h2>
+  <ul class="education">
+  <li class="cano">
+    ${object.education[0]}
+  </li>
+  <li> <img src="${object.image.counter}" alt="counter image">${object.education[1]}</li>
+  <li> <img src="${object.image.counter}" alt="counter image">${object.education[2]}</li>
+  </ul>
+  <p>
+    ${object.description}
+  </p>
+  <ul class="tech">
+      <li>
+        ${object.technologies[0]}
+      </li>
+      <li>
+         ${object.technologies[1]}
+      </li>
+      <li>
+         ${object.technologies[2]}
+      </li>
+      <li>
+        ${object.technologies[3]}
+      </li>
+  </ul>
+  <button data-id="${object.id}" class="button" type="button">
+    ${object.button}
+  </button>
+  </div>
+  </div>`;
+});
+
+// Add a popCard or remove popup card when going to see live or see source
+function pageTransition() {
+  if (windowPopup.classList.contains('pop-body')) {
+    windowPopup.classList.remove('pop-body');
+  } else {
+    windowPopup.classList.add('pop-body');
+    sections.forEach((sect) => {
+      sect.classList.toggle('active');
+    });
+  }
+}
+// Removes the Popup on click close button and Navigate to see live and see source on click
+
+
+// Loads the popup card on click
+bodyButtons.addEventListener('click', (e) => {
+  const buttonClass = e.target.getAttribute('data-id');
+  popup.forEach((object) => {
+    if (buttonClass === object.id) {
+      windowPopup.innerHTML = `<div id="${object.id}" class="pop-up">
+       <div class="pop-head">
+         <h2 class="ton-del">
+         ${object.title}
+         </h2>
+         <button type="button" class="cancel seeButtons">
+           ${object.cancel}
+         </button>
+       </div>
+       <ul class="education">
+         <li class="cano">
+           ${object.education[0]}
+         </li>
+         <li> <img src="${object.image.counter}" alt="counter image">${object.education[1]}</li>
+         <li> <img src="${object.image.counter}" alt="counter image">${object.education[2]}</li>
+       </ul>
+       <div>
+         <img class="pop-img" src="${object.image.mainImg}" alt="Snapshoot Portfolio image">
+       <div>
+         <div class="pop-bottom">
+           <p class="pop-para">
+             ${object.description}
+           </p>
+           <div class="sect-butns">
+             <div>
+               <ul class="tech javaht">
+                 <li>
+                     ${object.technologies[0]}
+                 </li>
+                 <li>
+                     ${object.technologies[1]}
+                 </li>
+                 <li>
+                     ${object.technologies[2]}
+                 </li>
+             </ul>
+             <ul class="tech bootrap">
+                 <li>
+                     ${object.technologies2[0]}
+                 </li>
+                 <li>
+                     ${object.technologies2[1]}
+                 </li>
+                 <li>
+                     ${object.technologies2[2]}
+                 </li>
+             </ul>
+             </div>
+             <hr class="single-line">
+             <nav class="pop-nav">
+             <a class="navii1" href="${object.navigation.live}" target="_blank">
+               <button class="pop-btn btn1 seeButtons" type="button">
+               ${object.butn1}
+                 <img class="seeButtons" src="${object.image.liveIcon}" alt="live-icon">
+               </button>
+             </a>
+             <a class="navii2" href="${object.navigation.source}" target="_blank">
+               <button class="pop-btn btn2 seeButtons"  type="button">
+               ${object.butn2}
+               <img class="seeButtons" src="${object.image.sourceIcon}" alt="github-icon">
+               </button>
+             </a>
+             </nav>
+           </div>
+         </div>
+       </div>
+      </div>
+     </div>`;
+      pageTransition();
+    }
+  });
+});
